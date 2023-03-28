073bd0144dc142acae3e4b2b00e25fff2ab4ba9b
kgp82 <grandpierre.kevin@gmail.com>
2023-03-28 23:42:54 +0200

    modify limit for maxRate

--- middleware/express-rate-limit.js
+++ middleware/express-rate-limit.js
@@ -5,7 +5,7 @@ const expressRateLimit = require('express-rate-limit');
 // Configuration de la limite de débit/user
 const maxRate = expressRateLimit({
                                      windowMs: 15 * 60 * 1000, // 15 minutes
-                                     max: 500, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+                                     max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
                                      standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
                                      legacyHeaders: false, // Disable the `X-RateLimit-*` headers
                                  })
